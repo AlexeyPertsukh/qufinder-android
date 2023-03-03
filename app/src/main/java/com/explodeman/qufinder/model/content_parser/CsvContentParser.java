09d0549b785928fc5cf985b9e7930619951a088e
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2023-03-03 11:24:02 +0200

    features: modify margin in layout

--- app/src/main/java/com/explodeman/qufinder/model/content_parser/CsvContentParser.java
+++ app/src/main/java/com/explodeman/qufinder/model/content_parser/CsvContentParser.java
@@ -6,33 +6,36 @@ import java.util.List;
 
 public class CsvContentParser implements ContentParser {
 
-  private final String delimiter;
-  private final int minArrayContentSize;
-  private final int maxArrayContentSize;
+    private final String delimiter;
+    private final int minArrayContentSize;
+    private final int maxArrayContentSize;
 
-  public CsvContentParser(String delimiter, int arrayContentSize) {
-    this(delimiter, arrayContentSize, arrayContentSize);
-  }
-
-  public CsvContentParser(String delimiter, int minArrayContentSize, int maxArrayContentSize) {
-    this.delimiter = delimiter;
-    this.minArrayContentSize = minArrayContentSize;
-    this.maxArrayContentSize = maxArrayContentSize;
-  }
-
-  @Override
-  public List<String[]> parse(List<String> strings) {
-    List<String[]> result = new ArrayList<>();
-    for (String string : strings) {
-      String[] arrayContent = string.split(delimiter);
-      int arrayContentSize = arrayContent.length;
-
-      if (arrayContentSize < minArrayContentSize || arrayContentSize > maxArrayContentSize) {
-        throw new IllegalArgumentException("Illegal array content size: " + arrayContentSize);
-      }
-
-      result.add(arrayContent);
+    public CsvContentParser(String delimiter, int arrayContentSize) {
+        this(delimiter, arrayContentSize, arrayContentSize);
     }
-    return result;
-  }
+
+    public CsvContentParser(String delimiter, int minArrayContentSize, int maxArrayContentSize) {
+        this.delimiter = delimiter;
+        this.minArrayContentSize = minArrayContentSize;
+        this.maxArrayContentSize = maxArrayContentSize;
+    }
+
+    @Override
+    public List<String[]> parse(List<String> strings) {
+        List<String[]> result = new ArrayList<>();
+        for (String string : strings) {
+            String[] arrayContent = string.split(delimiter);
+            for (int i = 0; i < arrayContent.length; i++) {
+                arrayContent[i] = arrayContent[i].trim();
+            }
+            int arrayContentSize = arrayContent.length;
+
+            if (arrayContentSize < minArrayContentSize || arrayContentSize > maxArrayContentSize) {
+                throw new IllegalArgumentException("Illegal array content size: " + arrayContentSize);
+            }
+
+            result.add(arrayContent);
+        }
+        return result;
+    }
 }
